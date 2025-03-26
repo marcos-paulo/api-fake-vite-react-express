df161f3607b54d1dd9e873f40b69b1f0f3be4ca7
Marcos Paulo De Almeida Lima <marcospaulo.mpa@gmail.com>
2025-03-26 11:13:40 -0300

    incluir descrição no endpoint

--- src/client/components/ListEndpoints.tsx
+++ src/client/components/ListEndpoints.tsx
@@ -10,9 +10,28 @@ export const ListEndpoints = ({
   onToggleEndpoint,
 }: ListEndpointsProps) => {
   return (
-    <ul>
+    <ul
+      style={{
+        border: "1px solid green",
+        padding: "5px 5px 0 5px",
+        textDecoration: "none",
+      }}
+    >
       {endpoints?.listEndpoints.map((endpoint) => (
-        <li key={endpoint.localhostAddress}>
+        <li
+          key={endpoint.localhostAddress}
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            padding: "5px",
+            marginBottom: "5px",
+            border: "1px solid orange",
+            // height: "50px",
+          }}
+        >
+          <span style={{ flex: "1 100%" }}>{endpoint.description}</span>
+
           <input
             type="checkbox"
             checked={endpoint.enabled}
